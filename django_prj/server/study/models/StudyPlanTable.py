0c0be59f7654628d9ffa650d97d981146f9869c8
gn <xieyixingwei@gmail.com>
2021-04-28 22:19:46 +0800

    update

--- django_prj/server/study/models/StudyPlanTable.py
+++ django_prj/server/study/models/StudyPlanTable.py
@@ -9,7 +9,7 @@ class StudyPlanTable(models.Model):
     """
     id = models.AutoField(primary_key=True)
     foreignUser = models.OneToOneField(to=UserTable, related_name='studyPlan', on_delete=models.CASCADE)
-    once_words = models.IntegerField(null=True, blank=True)
+    onceWords = models.IntegerField(null=True, blank=True)
     onceSentences = models.IntegerField(null=True, blank=True)
     onceGrammers = models.IntegerField(null=True, blank=True)
     vocabularies = JSONFieldUtf8(null=True, blank=True) # 单词本名字 [a,b]
