624dd23291b84537bdb1ba101265276b0aba4beb
Gao Nian <1198759776@qq.com>
2021-05-15 00:21:51 +0800

    update

--- flutter_prj/lib/widgets/custom_table.dart
+++ flutter_prj/lib/widgets/custom_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_prj/widgets/row_space.dart';
 
 
 class CustomTable extends StatelessWidget {
@@ -43,6 +44,7 @@ class _CustomTableState extends State<CustomTable> {
   @override
   Widget build(BuildContext context) =>
     Column(
+
       children: [
         title,
         header,
@@ -51,39 +53,86 @@ class _CustomTableState extends State<CustomTable> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
+          children: <Widget>[
             IconButton(
-              icon: Icon(Icons.arrow_back_ios, size: 20,),
+              icon: Icon(Icons.arrow_back_ios, size: 20, color: (index > 1) ? Colors.blueAccent : Colors.black26,),
+              splashRadius: 17,
               onPressed: () => goto != null && (index > 1) ? goto(index - 1) : null,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: _indexs(),
-            ),
+            )
+          ] + _indexs() + <Widget>[
             IconButton(
-              icon: Icon(Icons.arrow_forward_ios, size: 20,),
+              icon: Icon(Icons.arrow_forward_ios, size: 20, color: (index < count) ? Colors.blueAccent : Colors.black26),
+              splashRadius: 17,
               onPressed: () => goto != null && (index < count) ? goto(index + 1) : null,
             ),
+          ].where((e) => e != null).toList(),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('总页数 $count', style: TextStyle(fontSize: 17, color: Colors.black38)),
+            SizedBox(width: 17,),
+            Text('每页', style: TextStyle(color: Colors.black38, fontSize: 17),),
+            SizedBox(width: 8,),
             DropdownButton(
-              elevation: 10,
               value: perPage,
-              items: perPages.map((e)=>DropdownMenuItem(child: Text('$e'), value: e,)).toList(),
+              isDense: true,
+              items: perPages.map((e)=>DropdownMenuItem(child: Text('$e', style: TextStyle(fontSize: 17, color: Colors.black38),), value: e,)).toList(),
               onChanged: (v) => perPageChange != null ? perPageChange(v) : null,
               underline: Divider(height:1, thickness: 1),
             ),
-          ].where((e) => e != null).toList(),
+          ]
         )
       ].where((e) => e != null).toList(),
     );
 
   List<Widget> _indexs() {
-    var indexs = List<Widget>.generate(count, (i) =>
+    final maxCount = 6;
+    final maxCountHalf = maxCount / 2;
+    final counts = count < maxCount ? count : maxCount;
+    num start = 1;
+    if(index - maxCountHalf > 0 && index + maxCountHalf <= count) {
+      start = index - maxCountHalf;
+    } else if(index - maxCountHalf <= 0) {
+      start = 1;
+    }else if(index + maxCountHalf > count) {
+      start = count - maxCount + 1;
+    }
+    var indexs = List<Widget>.generate(counts, (i) =>
                 TextButton(
-                  child: Text('${i+1}'),
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(index==(i+1) ? Colors.blue[100] : Colors.transparent)),
-                  onPressed: () => goto != null ? goto(i+1) : null,
+                  child: Text('${i+start}', style: TextStyle(fontSize: 17, color: Colors.black38),),
+                  style: TextButton.styleFrom(
+                    backgroundColor: index==(i+start) ? Colors.blue[100] : Colors.transparent,
+                    padding: EdgeInsets.only(left: 4, right: 4),
+                    minimumSize: Size(30, 30),
+                  ),
+                  onPressed: () => goto != null ? goto(i+start) : null,
                 )
               );
+        if(start > 1) {
+          indexs.insert(0,
+                  TextButton(
+                    child: Text('1', style: TextStyle(fontSize: 17, color: Colors.black38),),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.only(left: 4, right: 4),
+                      minimumSize: Size(30, 30),
+                    ),
+                    onPressed: () => goto != null ? goto(1) : null,
+                  ));
+          indexs.insert(1,Text('......', style: TextStyle(fontSize: 17, color: Colors.black38),),);
+        }
+        if(index + maxCountHalf < count) {
+          indexs.add(Text('......', style: TextStyle(fontSize: 17, color: Colors.black38),),);
+          indexs.add(
+                  TextButton(
+                    child: Text('$count', style: TextStyle(fontSize: 17, color: Colors.black38),),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.only(left: 4, right: 4),
+                      minimumSize: Size(30, 30),
+                    ),
+                    onPressed: () => goto != null ? goto(count) : null,
+                  ));
+        }
     return indexs;
   }
 
