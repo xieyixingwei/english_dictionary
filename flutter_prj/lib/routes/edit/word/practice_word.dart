fe3d71504a2aa68b5092bb52f68c434003207795
Gao Nian <1198759776@qq.com>
2021-05-22 18:26:57 +0800

    auto practice word

--- flutter_prj/lib/routes/edit/word/practice_word.dart
+++ flutter_prj/lib/routes/edit/word/practice_word.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:audioplayers/audioplayers_web.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_prj/serializers/index.dart';
@@ -16,7 +18,24 @@ class PracticeWord extends StatefulWidget {
 
 class _PracticeWordState extends State<PracticeWord> {
   num index = 0;
+  bool auto = false;
+  bool cycle = true;
   WrappedPlayer _audioPlayer = WrappedPlayer();
+  final _style = TextStyle(fontSize: 14, color: Colors.black45);
+  Timer timer;
+
+  void _timerCallback(Timer t) {
+    _next();
+    if(_curWord.audioUsMan == null) return; _audioPlayer.setUrl(_curWord.audioUsMan); _audioPlayer.start(0);
+    if(cycle == false && (index == widget.words.length - 1)) {timer.cancel(); auto = false;}
+    setState((){});
+  }
+
+  @override
+  void dispose() {
+    if(timer != null && timer.isActive) timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) =>
@@ -34,6 +53,39 @@ class _PracticeWordState extends State<PracticeWord> {
           icon: Icon(Icons.arrow_back_ios, color: Colors.black54),
           onPressed: ()=> Navigator.pop(context),
         ),
+        centerTitle: false,
+        actions: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('循环', style: _style,),
+              SizedBox(width: 3,),
+              Switch(
+                value: cycle,
+                onChanged: (v) => setState(() => cycle = v),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('自动', style: _style,),
+              SizedBox(width: 3,),
+              Switch(
+                value: auto,
+                onChanged: (v) {
+                  if(v) {
+                    timer = Timer.periodic(Duration(seconds: 3), _timerCallback);
+                    if(_curWord.audioUsMan == null) return; _audioPlayer.setUrl(_curWord.audioUsMan); _audioPlayer.start(0);
+                  } else {
+                    timer.cancel();
+                  }
+                  setState(() {auto = v;});
+                },
+              ),
+            ],
+          ),
+        ],
       ),
       body: Center(
         child: Container(
@@ -46,7 +98,7 @@ class _PracticeWordState extends State<PracticeWord> {
             children: [
               _word,
               _detail(context),
-              _goto,
+              auto ? null : _goto,
             ],
           ),
         )
@@ -79,19 +131,32 @@ class _PracticeWordState extends State<PracticeWord> {
         ) : null,
       ],
     );
-  
+
+  void _next() {
+    if(cycle && (index == widget.words.length - 1)) index = 0;
+    else if(index < (widget.words.length - 1)) index += 1;
+  }
+  void _previous() {
+    if(cycle && index == 0) index = widget.words.length - 1;
+    else if(index > 0) index -= 1;
+  }
   WordSerializer get _curWord => widget.words[index];
   WordSerializer get _preWord {
     num preIndex = index;
-    if(index == 0) preIndex = widget.words.length - 1;
-    else preIndex = index - 1;
+    if(cycle && index == 0) preIndex = widget.words.length - 1;
+    else if(index > 0) preIndex = index - 1;
+    else preIndex = 0;
     return  widget.words[preIndex];
   }
 
   Widget _detail(BuildContext context) =>
-    TextButton(
-      child: Text('detail', style: TextStyle(fontSize: 17),),
-      onPressed: () => Navigator.pushNamed(context, '/show_word', arguments: {'title': '', 'word': _curWord}),
+    IconButton(
+      icon: Icon(Icons.more_horiz),
+      onPressed: () async {
+        if(timer != null && timer.isActive) timer.cancel();
+        await Navigator.pushNamed(context, '/show_word', arguments: {'title': '', 'word': _curWord});
+        if(auto) timer = Timer.periodic(Duration(seconds: 3), _timerCallback);
+      },
     );
 
   Widget get _goto =>
@@ -101,20 +166,12 @@ class _PracticeWordState extends State<PracticeWord> {
         IconButton(
           icon: Icon(Icons.arrow_back_ios, size: 30, color: Colors.blueAccent,),
           splashRadius: 17,
-          onPressed: () {
-            if(index == 0) index = widget.words.length - 1;
-            else index -= 1;
-            setState(() {});
-          },
+          onPressed: () => setState(() => _previous()),
         ),
         IconButton(
           icon: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.blueAccent),
           splashRadius: 17,
-          onPressed: () {
-            if(index == widget.words.length - 1) index = 0;
-            else index += 1;
-            setState(() {});
-          },
+          onPressed: () => setState(() => _next()),
         ),
       ],
     );
